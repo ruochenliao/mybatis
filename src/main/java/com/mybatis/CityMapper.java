a777ddb19df82853fbd33b73d96fcce3c7f1c0c9
john <lrc272317227@gmail.com>
2021-12-19 22:38:39 +0800

    使用 mybatis generator

---
+++ src/main/java/com/mybatis/CityMapper.java
@@ -0,0 +1,11 @@
+package com.mybatis;
+
+public interface CityMapper {
+
+    /**
+     * revised by steven
+     *
+     * @mbggenerated Sun Dec 19 16:52:06 CST 2021
+     */
+    CityDTO query(Long id);
+}
--- src/main/java/com/mybatis/MyController.java
+++ src/main/java/com/mybatis/MyController.java
@@ -1,6 +1,5 @@
 package com.mybatis;
 
-import com.mybatis.mapper.CityMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
